177e9f2d0f81f29f3b7eb1d41c7b58e97bc8badb
ramalac <ramalac01@gmail.com>
2023-10-07 21:05:10 +0200

    Constructeur de Soldat

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include "Controller.hpp"
-#include <SFML/System.hpp>
+//#include "Controller.hpp"
+//#include <SFML/System.hpp>
+#include "Soldat.hpp"
 
 int main()
 {
-
+    /*
     Controller *controller = Controller::getInstance();
 
     controller->InitFenetre(1920 / 2, 1080 / 2);
@@ -13,7 +14,24 @@ int main()
     controller->dessiner(shape);
     controller->afficherFenetre();
 
-    sf::sleep(sf::seconds(1));
-
+    sf::sleep(sf::seconds(5));
+    */
+    std::vector<sf::Vector2f> positions = std::vector<sf::Vector2f>();
+    positions.push_back(sf::Vector2f(1.5,3.5));
+    positions.push_back(sf::Vector2f(2.3,3.4));
+    positions.push_back(sf::Vector2f(5.4,1.3));
+    positions.push_back(sf::Vector2f(1.6,3.5));
+   
+    std::vector<sf::Vector2i> links = std::vector<sf::Vector2i>();
+    links.push_back(sf::Vector2i(0,1));
+    links.push_back(sf::Vector2i(0,2));
+    links.push_back(sf::Vector2i(0,3));
+    links.push_back(sf::Vector2i(2,3));
+    links.push_back(sf::Vector2i(1,3));
+    
+    Map map = Map(positions, links);
+    
+    Soldat soldat = Soldat(0, 5, 1, map);
+   
     return 0;
 }
