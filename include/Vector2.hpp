514f9a269d261cf33acb66966f386ca49918f00a
ramalac <ramalac01@gmail.com>
2023-10-07 14:19:27 +0200

    WIP

---
+++ include/Vector2.hpp
@@ -0,0 +1,14 @@
+#ifndef __VECTOR2_HPP__
+#define __VECTOR2_HPP__
+
+class Vector2 {
+
+public:
+    double x,y;
+
+public:
+    Vector2():x(0),y(0) {}
+    Vector2(double a, double b):x(a),y(a) {}    
+};
+
+#endif
