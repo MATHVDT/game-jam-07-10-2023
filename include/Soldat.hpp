1d5a7bb078d02c78309f24b85dc62cf95290146d
ramalac <ramalac01@gmail.com>
2023-10-08 17:26:39 +0200

    first version of 3 offset movement

--- include/Soldat.hpp
+++ include/Soldat.hpp
@@ -11,22 +11,27 @@ class Soldat : public Entite {
 
 private:
     bool alive;
+    int offset;
     sf::Vector2f position;
-    sf::Vector2f speed;
+    std::vector<sf::Vector2f> speed;
     float speed_coeff;
     std::vector<sf::Vector2f> destination;
 
     std::tuple<float, std::vector<sf::Vector2f>> Pathfinder(std::vector<sf::Vector2f> chemin, float len, int pos, int dest, Map map, std::vector<int> id_visited);
+    sf::Vector2f GetTangent(sf::Vector2f u);
+    sf::Vector2f RotatePoint(sf::Vector2f center, float angle, sf::Vector2f point, float det);
     
 public:
-    Soldat(int pos, float s, int dest, Map map, Entite::Type type, const sf::Vector2f &scale):alive(true),position(map.map[pos]->position),speed(sf::Vector2f()),speed_coeff(s),destination(std::vector<sf::Vector2f>()),Entite(type, scale) {
+    Soldat(int offs, int pos, float s, int dest, Map map, Entite::Type type, const sf::Vector2f &scale):offset(offs),alive(true),position(map.map[pos]->position),speed(std::vector<sf::Vector2f>()),speed_coeff(s),destination(std::vector<sf::Vector2f>()),Entite(type, scale) {
         destination = std::get<std::vector<sf::Vector2f>>(Pathfinder(std::vector<sf::Vector2f>(), 0, pos, dest, map, std::vector<int>()));
         destination.erase(destination.begin());
-        SetNewSpeed();
+        SetSpeed();
+        SetOffset();
         }
     void Avancer(float time);
-    void SetNewSpeed();
+    void SetSpeed();
     void Update();
+    void SetOffset();
 };
 
 #endif
